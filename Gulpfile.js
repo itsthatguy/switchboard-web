02f3ed08f609a65ec58b093f1c190db414f94c38
Kevin Altman <kevin@itsthatguy.com>
2014-04-02 22:57:13 -0400

    adding gulp and stylus to the project

---
+++ Gulpfile.js
@@ -0,0 +1,26 @@
+// Gulpfile.js
+// Require the needed packages
+var gulp = require('gulp');
+var stylus = require('gulp-stylus');
+
+var paths = {
+  css: ['./app/css/main.styl']
+};
+
+// Get one .styl file and render
+gulp.task('default', function () {
+  console.log("default task does nothing, hombre.")
+});
+
+// Get and render all .styl files recursively
+gulp.task('stylus', function () {
+  gulp.src(paths.css)
+    .pipe(stylus())
+    .pipe(gulp.dest('./.generated/css'));
+});
+
+gulp.task('watch', function() {
+  gulp.watch(paths.css, ['stylus']);
+});
+
+gulp.task('default', ['stylus']);
