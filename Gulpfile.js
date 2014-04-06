d9cc89493ec0b92ef08646a3e22df2e758db1376
Kevin Altman <kevin@itsthatguy.com>
2014-04-06 18:08:04 -0400

    ugh

--- Gulpfile.js
+++ Gulpfile.js
@@ -21,6 +21,25 @@ var paths = {
   assetsOutput: './.generated/'
 };
 
+var testFiles = [
+  '.generated/js/app.js',
+  'test/client/*.js'
+];
+
+
+gulp.task('test', function() {
+  // Be sure to return the stream
+  return gulp.src(testFiles)
+    .pipe(karma({
+      configFile: 'karma.conf.js',
+      action: 'run'
+    }))
+    .on('error', function(err) {
+      // Make sure failed tests cause gulp to exit non-zero
+      throw err;
+    });
+});
+
 
 gulp.task('default', function () {
   console.log("default task does nothing, hombre.")
