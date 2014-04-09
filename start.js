b4d698d210a8d9d7b19a063e14df72dd66d78d1d
Kevin Altman <kevin@itsthatguy.com>
2014-04-09 18:21:02 -0400

    improving the start script to handle cleaning up of process when one fails

--- start.js
+++ start.js
@@ -1,26 +1,44 @@
 #!/usr/bin/env node
 
+var util = require("util");
 var path = require('path');
 var spawn = require('win-spawn');
 
 
 basedir = path.join(__dirname, "node_modules", ".bin");
 
+var commandArray = [];
+
+var die = function(cmd) {
+  i = commandArray.indexOf(cmd);
+  commandArray.splice(i, 1);
+
+  for (var i = 0; i < commandArray.length; i++) {
+    commandArray[i].kill();
+  }
+}
 
 var runCommand = function(command, args) {
 
   var cmd = spawn(path.join(basedir, command), args);
+  commandArray.push(cmd);
 
   cmd.stdout.setEncoding('utf8');
   cmd.stdout.on('data', function(data) {
-    console.log(data);
+    util.print(data);
   });
   cmd.stderr.setEncoding('utf8');
   cmd.stderr.on('data', function(data) {
-    console.log(data);
+    util.print(data);
   });
   cmd.on('close', function(code) {
-    console.log('Child process exited with code', code);
+    util.print('Child process exited with code', code, "\n");
+    die(cmd);
+  });
+
+  cmd.on('exit', function(code) {
+    util.print('Child process exited with code', code, "\n");
+    die(cmd);
   });
 }
 
